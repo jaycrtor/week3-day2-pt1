e338dc615676418257f172ea8dc5964420a6099b
Jay <nhocchuot1995@gmail.com>
2022-10-11 11:04:01 -0400

    problem1 fisnished

--- problems/02-sum-array.js
+++ problems/02-sum-array.js
@@ -5,15 +5,30 @@ include a mix of positive and negative integers!
 
 Examples:
 
-sumArray([1, 2, 3]); //  6
-sumArray([0, 1, -3]); //  -2
-sumArray([1, 2, 3, 4, 5]); // 15
+
 ***********************************************************************/
 
 function sumArray(arr) {
-  // Your code here
+  for(let i in arr){
+    let num = arr[i]
+    if(arr.length == 0) return 0;
+    console.log(num)
+    if(arr.length > 0){
+      return num + sumArray(arr,i+1)
+    }
+
+
+
+
+  }
+
+
+
 }
-  
+console.log(sumArray([1, 2, 3])); //  6
+sumArray([0, 1, -3]); //  -2
+sumArray([1, 2, 3, 4, 5]); // 15
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumArray;
